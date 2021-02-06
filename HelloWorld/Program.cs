fae04cadc2309f0b62dacfcacf90e109c31ff031
akf2000 <30558056+akf2000@users.noreply.github.com>
2021-02-06 17:12:30 +0000

    strings

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,7 +6,15 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World! asif here nice to see you!");
+            var name = "asif"; // this is a comnment
+
+            string string1 = "string1";
+            string string2 = "string2";
+            string string3 = string1 + string2; // concatonate 2 strings
+            string greettemplate = "hello **NAME**!";
+            string greet4 = greettemplate.Replace("**NAME**", string1);
+
+            Console.WriteLine(greet4); 
         }
     }
 }
